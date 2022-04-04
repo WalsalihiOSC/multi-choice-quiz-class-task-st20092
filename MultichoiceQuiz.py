b74848136e0c8041ea1addf09f425e369851203f
st20092 <90332153+st20092@users.noreply.github.com>
2022-04-05 11:39:01 +1200

    Imported Tkinter and frames
    Created mainframe and answerframe

--- MultichoiceQuiz.py
+++ MultichoiceQuiz.py
@@ -1,3 +1,10 @@
 #week 9 Multi-choice Quiz
+from tkinter import *
 
 class MultiQuizGUI:
+
+    def __init__(self, parent):
+        mainframe = Frame(parent)
+        mainframe.grid()
+        answersframe = Frame(mainframe)
+        answersframe.grid(row = 1)
